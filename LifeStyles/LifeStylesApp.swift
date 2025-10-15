6ac59f47ae1e823ffb3e50fa6bffb5a847a015a8
sezginpak <sezginpaksoy@hotmail.com>
2025-10-15 15:45:29 +0300

    Initial Commit

---
+++ LifeStyles/LifeStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeStylesApp.swift
+//  LifeStyles
+//
+//  Created by sezgin paksoy on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LifeStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
